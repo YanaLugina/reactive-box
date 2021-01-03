b9ed6827d27d5b000432622b654846f93709afef
YanaLugina <yana4961@gmail.com>
2021-01-04 05:51:32 +1000

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import style from './App.css';
+
+function App() {
+  return (
+    <div className={style.App}>
+      <header className={style.AppHeader}>
+          erfe
+        <button>Push</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
